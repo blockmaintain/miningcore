016f7ebcf0cc3ae547f654f809a09b6225548463
Joshua Longhi <joshualonghi@gmail.com>
2018-06-07 17:16:25 -0400

    eth pool config added music

--- src/MiningCore/Blockchain/Ethereum/EthereumPool.cs
+++ src/MiningCore/Blockchain/Ethereum/EthereumPool.cs
@@ -43,7 +43,7 @@ using Newtonsoft.Json;
 
 namespace MiningCore.Blockchain.Ethereum
 {
-    [CoinMetadata(CoinType.ETH, CoinType.ETC, CoinType.EXP, CoinType.ELLA, CoinType.CLO)]
+    [CoinMetadata(CoinType.ETH, CoinType.ETC, CoinType.EXP, CoinType.ELLA, CoinType.CLO, CointType.MUSIC)]
     public class EthereumPool : PoolBase
     {
         public EthereumPool(IComponentContext ctx,
